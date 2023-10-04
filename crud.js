a1930f11aea724cdabeb16b7c69becd161d010fc
aishwaryak-01 <133705983+aishwaryak-01@users.noreply.github.com>
2023-10-04 23:31:42 +0530

    crud.js

--- crud.js
+++ crud.js
@@ -1,7 +1,6 @@
 const form=document.getElementById('my-form');
 const ul=document.getElementById('users');
 
-
 //Creating new Users - POST
 form.addEventListener('submit', addProducts)
 function addProducts(e)
@@ -10,9 +9,7 @@ function addProducts(e)
     const input1=document.getElementById('price').value;
     const input2=document.getElementById('username').value;
     const obj = { input1, input2 }
-
-axios
-    .post('https://crudcrud.com/api/58651c7aae85450e9b27fb91569ff3b3/RESTAPIProject', obj)
+axios.post('https://crudcrud.com/api/091cd7f34e3d47e1915343edc91c5c22/RESTAPIProject', obj)
     .then( (res) =>
     {
             showOutput(res.data)
@@ -21,42 +18,33 @@ axios
     {
             console.log(err);
     })
-
     document.getElementById('price').value='';
     document.getElementById('username').value='';
 }
 
-
-
 //Show result in Screen
 function showOutput(obj)
 {
-    ul.innerHTML = ul.innerHTML +
-    `<br><li id=${obj._id}>
-    ${obj.input1} - ${obj.input2} :
-    <button class='btn btn-danger' onclick=deleteProduct('${obj._id}')>Delete Product</button>
-    <button class='btn btn-primary' onclick=editProducts('${obj.input1}','${obj.input2}','${obj._id}')>Edit Product</button>
-    </li>`;
+    ul.innerHTML = ul.innerHTML + `<br><li id=${obj._id}>${obj.input1} - ${obj.input2} : <input type="hidden" class="form-control" value="${obj.input1}">
+        <button class='btn btn-danger' onclick=deleteProduct('${obj._id}')>Delete Product</button>
+        <button class='btn btn-primary' onclick=editProducts('${obj.input1}','${obj.input2}','${obj._id}')>Edit Product</button></li>`;
 }
 
-
-
-//Page refreshed show all users - GET
+//After page refreshed, show all users - GET
 async function displayAllUsers()
 {
     try
     {
-    const response = await axios.get('https://crudcrud.com/api/58651c7aae85450e9b27fb91569ff3b3/RESTAPIProject');
+    const response = await axios.get('https://crudcrud.com/api/091cd7f34e3d47e1915343edc91c5c22/RESTAPIProject');
     ul.innerHTML='';
     response.data.forEach(products => {
         showOutput(products)
-    }); }
-
+    });
+    }
     catch(error)
     {
         console.log(error)
     }
-
     finally
     {
         console.log('Got all the Products');
@@ -64,23 +52,25 @@ async function displayAllUsers()
 }
 window.addEventListener('DOMContentLoaded', displayAllUsers)
 
-
-
-
 //Deleting user from Screen & CrudCrud - DELETE
 function deleteProduct(delId)
 {
-    axios
-    .delete(`https://crudcrud.com/api/58651c7aae85450e9b27fb91569ff3b3/RESTAPIProject/${delId}`)
-    .then( (res) => {
-        removeUser(delId)
-    })
-    .catch( (err) => {
-        console.log(err);
-    })
+    var delPrice = parseFloat(document.getElementById(delId).querySelector('.form-control').value);
+    axios.delete(`https://crudcrud.com/api/091cd7f34e3d47e1915343edc91c5c22/RESTAPIProject/${delId}`)
+        .then((res) => {
+            removeUser(delId);
+            // Subtract the deleted product's price from the total value
+            if (!isNaN(delPrice))
+            {
+                total -= delPrice;
+                updateTotalDisplay(); // Update total value
+            }
+        })
+        .catch((err) =>
+        {
+            console.log(err);
+        });
 }
-
-
 
 //Remove user from Screen
 function removeUser(delId)
@@ -92,8 +82,6 @@ function removeUser(delId)
         }
 }
 
-
-
 //Edit users from Screen & CrudCrud - PUT
 function editProducts(input1, input2, editId)
 {
@@ -101,18 +89,12 @@ function editProducts(input1, input2, editId)
     document.getElementById('username').value=input2;
     const updateObj = { input1, input2 }
 
-    axios
-    .put(`https://crudcrud.com/api/58651c7aae85450e9b27fb91569ff3b3/RESTAPIProject/${editId}`, updateObj)
+    axios.put(`https://crudcrud.com/api/091cd7f34e3d47e1915343edc91c5c22/RESTAPIProject/${editId}`, updateObj)
     .then( (res) => {
                     deleteProduct(editId);
                     displayAllUsers();
                     })
     .catch( (err) => {
     console.log(err)
-})
+    })
 }
-
-
-
-
-
